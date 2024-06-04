75b2f00b8f6ed0fb69e516fc9847c71a520ce14e
benboaz1 <benedictboaz@gmail.com>
2024-06-04 09:24:44 -0500

    Random parameterized constructor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-/*The moon's gravity is about 17 percent that of the earth's. Write a program
-that computes your effective weight on the moon */
+/*This is a simple program about constructors*/
+
 
 import java.util.Scanner;
+class MyClass{
+    int  x ;
 
+    MyClass(int i){
+        x = i;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please enter your weight on Earth : ");
-        Scanner Earth_weight_input = new Scanner(System.in);
-        int Earth_weight = Earth_weight_input.nextInt();
-        double Moon_Weight = Earth_weight * .17;
-        System.out.println("Your weight on the moon is : " + Moon_Weight + " Lbs");
+    MyClass a = new MyClass(10);
+    System.out.println(a.x);
     }
 }
